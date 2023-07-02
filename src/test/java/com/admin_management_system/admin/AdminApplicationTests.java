48bdac8658778ab155355b200fc77e79ee1254b6
Diyana Perera <tdsperera@students.nsbm.ac.lk>
2023-07-02 15:04:35 +0530

    Initial commit

---
+++ src/test/java/com/admin_management_system/admin/AdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.admin_management_system.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
